ef908e20e1a5e2ecaeeff40dc745dbcc54e7ccc2
Jason J Simpson <jasonjamessimpson@gmail.com>
2018-12-21 10:51:09 +1300

    For loops finished and working

--- minesweeper.js
+++ minesweeper.js
@@ -1,28 +1,56 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = 
-  {cells:
+var board = {
+  cells:
     [
-    {row: 0, col: 0, isMine: false, isMarked: 0, hidden: 0, surroundingMines: 0},
-    {row: 0, col: 1, isMine: false, isMarked: 0, hidden: 0, surroundingMines: 0},
-    {row: 0, col: 2, isMine: false, isMarked: 0, hidden: 0, surroundingMines: 0},
-    {row: 1, col: 0, isMine: true, isMarked: 0, hidden: 0, surroundingMines: 0},
-    {row: 1, col: 1, isMine: false, isMarked: 0, hidden: 0, surroundingMines: 0},
-    {row: 1, col: 2, isMine: false, isMarked: 0, hidden: 0, surroundingMines: 0},
-    {row: 2, col: 0, isMine: true, isMarked: 0, hidden: 0, surroundingMines: 0},
-    {row: 2, col: 1, isMine: false, isMarked: 0, hidden: 0, surroundingMines: 0},
-    {row: 2, col: 2, isMine: true, isMarked: 0, hidden: 0, surroundingMines: 0}
+    {row: 0, col: 0, isMine: false, isMarked: false, hidden: true, surroundingMines: 0},
+    {row: 0, col: 1, isMine: false, isMarked: false, hidden: true, surroundingMines: 0},
+    {row: 0, col: 2, isMine: false, isMarked: false, hidden: true, surroundingMines: 0},
+    {row: 1, col: 0, isMine: true, isMarked: false, hidden: true, surroundingMines: 0},
+    {row: 1, col: 1, isMine: false, isMarked: false, hidden: true, surroundingMines: 0},
+    {row: 1, col: 2, isMine: false, isMarked: false, hidden: true, surroundingMines: 0},
+    {row: 2, col: 0, isMine: true, isMarked: false, hidden: true, surroundingMines: 0},
+    {row: 2, col: 1, isMine: false, isMarked: false, hidden: true, surroundingMines: 0},
+    {row: 2, col: 2, isMine: true, isMarked: false, hidden: true, surroundingMines: 0}
     ]
   }
+// Alternate board definition
+// var board = { cells: generateBoard(3)}  
+//   function generateBoard(size) {
+//     var cells = []
+//     for (i= 0; i< size; i++) {
+//       for (j=0; j< size; j++) {
+//       cell = {
+//         row: i,
+//         col: j, 
+//         isMine: Math.random()<0.20,0:1,
+//         hidden: true,
+//         isMarked: false,
+//         }
+//       cells.push(cell)
+//     }
+//   }   
+//   return cells
+// }
 
 function startGame () {
   for (var i = 0; i < board.cells.length; i++){
-  var result = countSurroundingMines(i) 
-  console.log('count:' + result)
-  return board.cells[i].surroundingMines = result
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   } 
-  // Don't remove this function call: it makes the game work!
+  function countSurroundingMines (cell) {
+    var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+    var count = 0
+    for (var i = 0; i < surrounding.length; i++){
+      if (surrounding[i].isMine === true) {
+        count++
+        }
+        else console.log("peanuts?")
+      }
+    return count
+  }
+  document.addEventListener("click", checkForWin);
+  document.addEventListener("contextmenu", checkForWin);
   lib.initBoard()
 }
 
@@ -31,6 +59,12 @@ function startGame () {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
+  for (var i = 0; i < board.cells.length; i++){
+    var check = board.cells[i]
+    if(!check.isMine && check.isMarked) {
+      return 
+    }
+
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
@@ -45,23 +79,3 @@ function checkForWin () {
 //
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
-function countSurroundingMines (cell) {
-  let count = 0
-  var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-  console.log('surrounding:' + surrounding)
-  // for loop
-  for (var i = 0; i < surrounding.length; i++) {
-  //if obj.isMine === isMine
-  // count ++
-  if (surrounding[i].isMine === true) {
-  return count++
-  }
-  else console.log("peanuts?")
-  }
-  console.log('cells that are mines:' + count)
-  return count
-}
-
-
-
-startGame()
